eb62169d7c45101092ac70c985f6125a81232b9b
nikhilbaloji <nikhilbaloji@gmail.com>
2025-11-04 11:43:43 +0530

    Update sys.py

--- sys.py
+++ sys.py
@@ -1,14 +1,16 @@
 import sys
-if lens(sys.argv) == 3:
-  script_name = sys.argv[0]
-  name = sys.argv[1]
-  rollno = sys.argv[2]
-  print("user provided input values:")
+
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("User provided input values:")
 else:
-  script_name = sys.argv[0]
-  name = "Nikhil"
-  rollno = "032"
-  print("No input given - using default values:")
-  print("Script Name:", script_name)
-  print("Student Name:", name)
-  print("Roll Number:", rollno)
+    script_name = sys.argv[0]
+    name = "Deepa"
+    rollno = "101"
+    print("No input given using default values:")
+
+print("Script Name:", script_name)
+print("Student Name:", name)
+print("Roll Number:", rollno)
